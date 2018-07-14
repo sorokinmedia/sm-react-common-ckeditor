e6b33d0ee457bd9d8536cbf6e38d44b4b5bba9a2
magarampage <magarampage@gmail.com>
2018-07-14 15:30:34 +0300

    tests fix

--- src/index.js
+++ src/index.js
@@ -54,14 +54,12 @@ class CKEditor extends Component {
 			this.editor.setData(this.props.value);
 		}
 	}
-
 	// componentWillUnmount() {
 	// 	this.editor.destroy()
 	// 		.catch(error => {
 	// 			console.log(error);
 	// 		});
 	// }
-
 	render() {
 		return (
 			<div className="form-group">
